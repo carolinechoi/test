22574ff0131b275d1edde658d1c48c830e9a8c61
Caroline <38522055+carolinechoi@users.noreply.github.com>
2019-07-24 09:48:47 -0500

    new updates

--- src/index.js
+++ src/index.js
@@ -4,15 +4,15 @@ import store from './store'
 import Vuetify from 'vuetify';
 import 'vuetify/dist/vuetify.min.css'
 import App from './App.vue';
-// import EventHub from 'vue-event-hub';
+import EventHub from 'vue-event-hub';
 
 let geoJsonCurrentGauges;
 let geoJsonGaugeDataForecast;
 
 Vue.use(Vuetify);
-// Vue.use(EventHub);
+Vue.use(EventHub);
 
-Vue.prototype.$eventHub = new Vue();
+// Vue.prototype.$eventHub = new Vue();
 
 let vm = new Vue({
     el: '#app',
